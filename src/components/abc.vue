4aaf8750f97719d854257aed62f9c1d010a74f05
zengjun <2592692956@qq.com>
2023-01-28 17:37:29 +0800

    vue多页面脚手架

---
+++ src/components/abc.vue
@@ -0,0 +1,26 @@
+<!-- abc -->
+<template>
+    <div class='abc'>
+        165498798789
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        };
+    },
+    created() {
+        console.log(123456)
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/pages/abc/abc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div>
+      88999999999赛季吉萨大赛季的
+    </div>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
